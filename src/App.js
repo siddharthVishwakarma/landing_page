732ce5be826e3405064af76b2a0221c69236a11f
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-06-22 16:26:57 +0530

    Section✅

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Section from "./components/Section.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Section />
     </>
   );
 }
