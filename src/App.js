b073ed39a2608c00c55c7680e19e7aaa4aaa7ac9
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-06-22 16:54:18 +0530

    ✅ Review

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Section from "./components/Section.jsx";
 import Pricing from "./components/Pricing.jsx";
+import Review from "./components/Review.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Section />
       <Pricing />
+      <Review />
     </>
   );
 }
