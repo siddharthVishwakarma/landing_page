0a5971e5c811a6640308b10bbfe9030887d95294
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-06-22 16:47:02 +0530

    ✅ Pricing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Section from "./components/Section.jsx";
+import Pricing from "./components/Pricing.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Section />
+      <Pricing />
     </>
   );
 }
