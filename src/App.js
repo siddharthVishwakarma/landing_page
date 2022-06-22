a17e5212e87d3891c57ab4f1867dc8d806e6415e
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-06-22 16:01:25 +0530

    Hero✅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
